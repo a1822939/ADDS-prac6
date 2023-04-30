47e8542af528e8ee3ea2b63941b97ea5677e76f3
a1822939 <a1822939@student.adelaide.edu.au>
2023-05-01 09:10:58 +0930

    f

--- BigNumCalc.cpp
+++ BigNumCalc.cpp
@@ -1,32 +1,90 @@
 #include "BigNumCalc.h"
 #include <string>
 
+BigNumCalc::BigNumCalc()
+{
+}
+BigNumCalc::~BigNumCalc()
+{
+}
 
-    BigNumCalc::BigNumCalc(){
+std::list<int> BigNumCalc::buildBigNum(std::string numString)
+{
+    std::list<int> numList;
 
+    for (int i = 0; i < numString.size(); i++)
+    {
+        int charInt = numString.at(i) - 48;
+        numList.push_back(charInt);
     }
-    BigNumCalc::~BigNumCalc(){
 
-    }
-
-    std::list<int> BigNumCalc::buildBigNum(std::string numString){
-        std::list<int> numList;
-        
-        for (int i = 0; i < numString.size(); i++)
+    return numList;
+}
+std::list<int> add(std::list<int> num1, std::list<int> num2)
+{
+    std::list<int> result;
+    int carry = 0;
+    while (!num1.empty() || !num2.empty() || carry)
+    {
+        int sum = carry;
+        if (!num1.empty())
         {
-            int charInt = numString.at(i)-48;
-            numList.push_back(charInt);
+            sum += num1.back();
+            num1.pop_back();
         }
+        if (!num2.empty())
+        {
+            sum += num2.back();
+            num2.pop_back();
+        }
+        result.push_front(sum % 10);
+        carry = sum / 10;
+    }
+    return result;
+}
 
-        return numList;
+std::list<int> sub(std::list<int> num1, std::list<int> num2)
+{
+    std::list<int> result;
+    int borrow = 0;
+    while (!num1.empty() || !num2.empty())
+    {
+        int sub = borrow;
+        if (!num1.empty())
+        {
+            sub += num1.back();
+            num1.pop_back();
+        }
+        if (!num2.empty())
+        {
+            sub -= num2.back();
+            num2.pop_back();
+        }
+        if (sub < 0)
+        {
+            sub += 10;
+            borrow = -1;
+        }
+        else
+        {
+            borrow = 0;
+        }
+        result.push_front(sub);
     }
-    std::list<int> BigNumCalc::add(std::list<int> num1, std::list<int> num2){
-        
-        
+    while (!result.empty() && result.front() == 0)
+    {
+        result.pop_front();
     }
-    std::list<int> BigNumCalc::sub(std::list<int> num1, std::list<int> num2){
+    return result;
+}
 
-    }
-    std::list<int> BigNumCalc::mul(std::list<int> num1, std::list<int> num2){
+std::list<int> mul(std::list<int> num1, std::list<int> num2)
+{
+    std::list<int> sum;
 
+    for (int i = 0; i < num2.front(); i++)
+    {
+        sum = add(num1, sum);
     }
+    return sum;
+}
